6b206d1b0b242081bc61d66dafdfeb0d7c9f5e83
Partha Sarker <parthasarker3@gmail.com>
2020-07-02 02:44:11 +0600

    5 octaves of piano keys(2,3,4,5,6) and question is now sheet note

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var sheet = true;
+    var currentSheetNote = '';
     var correct = 0, wrong = 0;
     var correctCount = $('#correct-count');
     var wrongCount = $('#wrong-count');
@@ -16,13 +18,28 @@ $(document).ready(function () {
         }
     });
 
-    $('.octave').click(function (e) { 
-        var values = e.target.className.split(' ');
-        if(values.includes('white-key') || values.includes('black-key')){
-            // console.log(e.target.className);
-            if(playButton.text() != 'Stop')
-                return;
-            if(values.includes(note.text())){
+    $('.octave').click(function (e) {
+        // if(playButton.text() != 'Stop')
+        //     return;
+        var jq_target = $(e.target);
+        var classList = [];
+        try {
+            classList = jq_target.attr('class').split(' ');            
+        } catch (error) {
+            jq_target = jq_target.parent();
+            classList = jq_target.attr('class').split(' ');            
+        }
+        if(classList[0] == 'octave')
+            return;
+        
+        var pianoNote = classList[1];
+        var octave = jq_target.parent().attr('class').split(' ')[1];
+        var finalNote = pianoNote + '/' + octave;
+        // console.log(finalNote);
+        
+
+        if(sheet == true){
+            if(finalNote == currentSheetNote){
                 correct++;
                 setTextFadeIn(correctCount, 'Correct: '+correct);
                 setRandomNote();
@@ -32,7 +49,9 @@ $(document).ready(function () {
                 setTextFadeIn(wrongCount, 'Wrong: '+wrong);
                 setRandomNote(note.text());
             }
+            return;
         }
+
     });
 
     function startPlaying(){
@@ -55,16 +74,95 @@ $(document).ready(function () {
         }
         var finalNote= '';
         var notes= 'ABCDEFG';
-        finalNote += notes.charAt(Math.floor(Math.random() * notes.length));
         var extras = '#b ';
-        finalNote += extras.charAt(Math.floor(Math.random() * extras.length));
-        finalNote = finalNote.trim();
+        
+        var pianoNote = notes.charAt(Math.floor(Math.random() * notes.length));
+        var extra = extras.charAt(Math.floor(Math.random() * extras.length));
+        
+        finalNote = (pianoNote + extra).trim();
         setTextFadeIn(note, finalNote);
+
+
+        var clefs = ['bass', 'treble'];
+        var octave = '';
+        var clef = clefs[Math.floor(Math.random() * clefs.length)];
+
+        if(clef == 'bass'){
+            octave = '234'.charAt(Math.floor(Math.random() * 3));
+            if(octave == '4' && pianoNote != 'C')
+                octave = '2';
+        }
+        else if(clef == 'treble'){
+            octave = '44556'.charAt(Math.floor(Math.random() * 3));
+        }
+
+        finalNote = pianoNote + '/' + octave;
+        currentSheetNote = finalNote;
+        drawNote(finalNote, clef, ' ');
     }
 
     function setTextFadeIn(element, text){
-        element.fadeOut( function() {
-            element.text(text).fadeIn();
-        });
+        element.text(text);
+        // element.fadeOut( function() {
+        //     element.text(text).fadeIn();
+        // });
     }
+
+    drawSheet();
+
+    // drawNote('b/6', 'treble', ' ');
+    // drawNote('c/4', 'bass', ' ');
+
+    
+    function drawSheet(){
+        const VF = Vex.Flow;
+        $('#sheet svg').remove();
+
+
+        var div = document.getElementById("sheet");
+        var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+        var context = renderer.getContext();
+        var stave = new VF.Stave(0, 15, 400);
+
+        stave.addClef('treble');
+        
+        stave.setContext(context).draw(); 
+    }
+
+    function drawNote(note, clef, accidental){
+        // console.log(note + ' ' + clef + ' ' + accidental);
+
+        const VF = Vex.Flow;
+        $('#sheet svg').remove();
+
+
+        var div = document.getElementById("sheet");
+        var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+        var context = renderer.getContext();
+        var stave = new VF.Stave(0, 15, 400);
+
+        stave.addClef(clef);
+        
+        stave.setContext(context).draw();
+
+        if(accidental != ' ')
+            note = new VF.StaveNote({clef: clef, keys: [note], duration: "1" }).addAccidental(0, new VF.Accidental(accidental));
+        else
+            note = new VF.StaveNote({clef: clef, keys: [note], duration: "1" });
+
+        var notes = [note];
+        
+        
+        var voice = new VF.Voice();
+        voice.addTickables(notes);
+        
+        // Format and justify the notes to 400 pixels.
+        var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+        
+        // Render voice
+        voice.draw(context, stave);
+    }
+
 });
